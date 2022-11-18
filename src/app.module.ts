99878a9be2f96068af06abad1f4aac80671ebb97
Jovanir Mateus de Almeida Lopes <jovanir.lopes@outlook.com>
2022-11-18 14:10:07 -0300

    DB config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig } from 'configs/db.config';
+import { TelephoneModule } from './telephone/telephone.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { dbConfig } from 'configs/db.config';
       inject: [ConfigService],
       useFactory: dbConfig,
     }),
+    TelephoneModule,
   ],
   controllers: [],
   providers: [],
