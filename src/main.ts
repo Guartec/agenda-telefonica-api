b4d0de107a662094c4f419b8522980e660106c9c
Jovanir Mateus de Almeida Lopes <jovanir.lopes@outlook.com>
2022-11-21 11:15:50 -0300

    Instaled validation modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (err) => {
+        throw new BadRequestException(err);
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
