ab30039fcf9ff905e3855e318bf424e4f059606f
Jovanir Mateus de Almeida Lopes <jovanir.lopes@outlook.com>
2022-11-21 11:16:17 -0300

    Updated imports

--- src/telephone/telephone.module.ts
+++ src/telephone/telephone.module.ts
@@ -1,9 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { TelephoneService } from './telephone.service';
 import { TelephoneController } from './telephone.controller';
+import { Telephone } from './entities/telephone.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Telephone])],
   controllers: [TelephoneController],
-  providers: [TelephoneService]
+  providers: [TelephoneService],
 })
 export class TelephoneModule {}
